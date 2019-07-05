190b21ad012c23d226f462b5fa17b30770654038
Thomas Ghysels <info@thomasg.be>
2019-07-05 12:35:29 +0200

    Add more random string helpers

--- random.mjs
+++ random.mjs
@@ -7,3 +7,25 @@ export function id(len = 10) {
     )
     .join('')
 }
+
+export function str62(len = 10) {
+  return Array.apply(null, Array(len))
+    .map(char62)
+    .join('')
+}
+
+export function str36(len = 10) {
+  return Array.apply(null, Array(len))
+    .map(() =>
+      Math.random()
+        .toString(36)
+        .charAt(5)
+    )
+    .join('')
+}
+
+export function char62() {
+  return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt(
+    Math.floor(Math.random() * 62)
+  )
+}
