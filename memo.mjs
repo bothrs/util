0a8fbbb69cf22dc7dc3f4e2e6596dfbc42fb9987
Thomas Ghysels <info@thomasg.be>
2019-09-17 12:27:46 +0200

    Add reset method to memo and optimist helper

--- memo.mjs
+++ memo.mjs
@@ -21,6 +21,7 @@ export function memo(func, timeout) {
   }
   // TODO: expire this?
   f.set = (key, value) => (cache[JSON.stringify(key)] = value)
+  f.reset = (a, b, c) => (cache[JSON.stringify([a, b, c])] = null)
   return f
 }
 
@@ -49,5 +50,6 @@ export function optimist(func, timeout = 60000) {
     return cache[key]
   }
   f.set = (key, value) => (cache[JSON.stringify(key)] = value)
+  f.reset = (a, b, c) => (cache[JSON.stringify([a, b, c])] = null)
   return f
 }
